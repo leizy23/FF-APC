46b3b8d8ee4030eb40ef4998b271f0a2676be710
leizy23 <leizy23@mails.tsinghua.edu.cn>
2025-02-23 20:34:05 +0800

    Add files via upload

---
+++ model in simulink/1SG1GFL1FFAPC9BUS/main.m
@@ -0,0 +1,26 @@
+kp1 = 5;
+kp2 = 5;
+kp3 = 5;
+
+Pmax1 = 1.122; % (0.7317e8)
+Pmax2 = 1.200; % (1.636e8)
+Pmax3 = 1.15;  %(0.8553e8)
+Pmin1 = 0.878;
+Pmin2 = 0.800;
+Pmin3 = 0.850;
+kv = 4;
+
+ts_pi = 5;
+Vnom_prim = 230e3;
+Pnom = 100e6;
+t_st = 5;
+
+ct = 2;
+% save('result60.mat','P','fre','V_mag');
+
+% Kf = 0.000001;
+% Tf = 1;
+% Ka = 20;
+% Ta = 0.2;
+% 
+% SM.One_Llq = 0.1969;
